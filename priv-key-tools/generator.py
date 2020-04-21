b0967485d86e48f84c98fb3fe2788fc7cd2ed06b
Milosz <gryfino@gmail.com>
2020-04-21 18:06:12 +0200

    Generate private keys from consecutive numbers, calculate public keys and check balances.

--- priv-key-tools/generator.py
+++ priv-key-tools/generator.py
@@ -1,16 +1,19 @@
 #import only neccessary functions
 from bitcoin import random_key,privkey_to_address
+from blockcypher import get_latest_block_height, get_total_balance, get_address_overview, from_satoshis
 
-#generate random key and assign it to the variable
-#private_key = random_key()
+#int("0x1", 16) #first private key
+#int("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140", 16) #last private key
 
-#check string as private key
-#private_key = int("0x1", 16) #first private key
-private_key = int("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364140", 16) #last private key
-
-#use generated private key to generate BTC address and assign it to variable
-address = privkey_to_address(private_key)
-
-#print both private key and it's public address
-print(private_key)
-print(address)
+for i in range(35):
+    #print(hex(i)) #show iterator in hex to reflect priv key format
+    priv_key = i #generate priv_key from int anyway
+    address = privkey_to_address(priv_key)
+    addr_overview = get_address_overview(address) #getting address data
+    #[print(key, value) for key, value in addr_overview.items()] #this would show dict with address info
+    for key,value in addr_overview.items(): #getting info from address overview
+        if key == 'final_balance': #check for funds in the address
+            if value > 0: #if any btc left on address, print details
+                print(priv_key)
+                print(address)
+                print(from_satoshis(value))
